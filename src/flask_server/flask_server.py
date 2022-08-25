f7e2a9d9939ae364eeea09d33d1fbbc0b17cce50
kingkonggrunt <little-kk@hotmail.com>
2022-08-26 02:35:26 +1000

    lint: line breaks

--- src/flask_server/flask_server.py
+++ src/flask_server/flask_server.py
@@ -8,14 +8,12 @@ def home():
     with open(".logs/word.log", 'r') as f:
         content = f.read()
         return Response(content, mimetype='text/plain')
-        
-    
+
+
 def run():
     app.run(host='0.0.0.0', port=8080)
-    
-    
+
+
 def keep_alive():
     t = Thread(target=run)
     t.start()
-
-    
