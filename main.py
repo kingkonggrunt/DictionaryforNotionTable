128ec24fa1f02d14393985531d3aa6e95ae2c6c2
kingkonggrunt <little-kk@hotmail.com>
2022-08-26 04:25:54 +1000

    feat: implemented formating for definitions

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pprint import pprint
 
 from src.dictionary import Dictionary
 from src.dictionary.interface import WordNotFoundError
-from src.notion import NotionDatabase, NotionPage
+from src.notion import NotionDatabase, NotionPage, builders
 from src.loggers import handlers
 from src.flask_server import flask_server
 
@@ -72,7 +72,7 @@ def main():
             # Find the page's word
             word = page.get_page_property('Name')['results'][0]['title']['plain_text']
     
-            # Attempt to find it's definition, and add it to the page
+            # Attempt to find it's definition. Write into logs whether definition was found
             try:
                 definition = Dictionary(word)
             except WordNotFoundError as exc:
@@ -81,7 +81,48 @@ def main():
                 continue
         
             append_to_file(".logs/word.log", f"{datetime.now()}: {word} -> found")
-            page.set_page_property('Definitions', page.rich_text(str(definition.meanings)))
+            
+            # Format the definition to look pretty on Notion
+            patch = builders.RichTextBuilder()
+            for i, pos in enumerate(definition.parts_of_speech):
+                p = getattr(definition, str(pos))
+                
+                
+                # start at a new line/paragraph if we are not at the first pos
+                if i != 0:
+                    patch.new_line()
+                    patch.new_line()
+
+                # Bold part of speech
+                patch.add_rich_text(text=p.part_of_speech.title(), bold=True)
+                
+                # list definitions
+                for i, single_definition in enumerate(p.definition):
+                    patch.new_line()    
+                    patch.add_rich_text(f"{i+1}. ", bold=True)
+                    patch.add_rich_text(single_definition['definition'])
+ 
+                    
+                # list synonyms and antonyms 
+                if p.synonyms:
+                    patch.new_line()
+                    patch.add_rich_text("synonyms: ", bold=True, italic=True)
+                    patch.add_rich_text(", ".join(p.synonyms))
+                
+                if p.antonyms:
+                    patch.new_line()
+                    patch.add_rich_text("antonyms: ", bold=True, italic=True)
+                    patch.add_rich_text(", ".join(p.antonyms))
+                    
+                # show example if exists
+                if p.example:
+                    patch.new_line()
+                    patch.add_rich_text(p.example, italic=True)
+                
+            
+            
+            # Add formatted definition to Notion
+            page.set_page_property('Definitions', patch.get_patch())
             
         # sleep for 15 minutes
         sleep(60*15)
